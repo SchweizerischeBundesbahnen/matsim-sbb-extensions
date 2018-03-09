0d809260ec94009decacb14edb671a86611989f8
Marcel Rieser <marcel.rieser@sbb.ch>
2018-03-09 14:02:52 +0100

    Add a custom MainModeIdentifier that is aware of intermodal access/egress.

--- src/main/java/ch/sbb/matsim/RunSBBExtension.java
+++ src/main/java/ch/sbb/matsim/RunSBBExtension.java
@@ -5,6 +5,7 @@
 package ch.sbb.matsim;
 
 import ch.sbb.matsim.mobsim.qsim.SBBQSimModule;
+import ch.sbb.matsim.routing.pt.raptor.IntermodalAwareRouterModeIdentifier;
 import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
@@ -12,8 +13,8 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.router.MainModeIdentifier;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.scoring.ScoringFunctionFactory;
 
 /**
  * Example script that shows how to use the extensions
@@ -40,6 +41,7 @@ public class RunSBBExtension {
 
 				// To use the fast pt router:
 				install(new SwissRailRaptorModule());
+				bind(MainModeIdentifier.class).to(IntermodalAwareRouterModeIdentifier.class);
 			}
 		});
 
