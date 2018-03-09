946744decc71c459c96280c83d970fd52130671c
Marcel Rieser <marcel.rieser@sbb.ch>
2018-03-09 12:59:14 +0100

    add our own TripRouter-Wrapper for SwissRailRouter. The default TransitRouterWrapper had problems with the intermodal access/egress legs, expecting all non-transit-walk legs to have a TransitRoute, which is not the case with intermodal access legs. Having our own wrapper also allows to perform some optimizations, like the optimized calculation of a transit route during routing instead of afterwards.

--- src/main/java/ch/sbb/matsim/routing/pt/raptor/SwissRailRaptorModule.java
+++ src/main/java/ch/sbb/matsim/routing/pt/raptor/SwissRailRaptorModule.java
@@ -4,8 +4,11 @@
 
 package ch.sbb.matsim.routing.pt.raptor;
 
+import com.google.inject.Key;
+import com.google.inject.name.Names;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.core.controler.AbstractModule;
-import org.matsim.pt.router.TransitRouter;
+import org.matsim.core.router.RoutingModule;
 
 /**
  * @author mrieser / SBB
@@ -15,9 +18,15 @@ public class SwissRailRaptorModule extends AbstractModule {
     @Override
     public void install() {
         if (getConfig().transit().isUseTransit()) {
-            bind(TransitRouter.class).toProvider(SwissRailRaptorFactory.class);
+            bind(SwissRailRaptor.class).toProvider(SwissRailRaptorFactory.class);
+            for (String mode : getConfig().transit().getTransitModes()) {
+                addRoutingModuleBinding(mode).toProvider(SwissRailRaptorRoutingModuleProvider.class);
+            }
             bind(RaptorRouteSelector.class).to(LeastCostRaptorRouteSelector.class);
+            addRoutingModuleBinding(TransportMode.transit_walk).to(Key.get(RoutingModule.class, Names.named(TransportMode.walk)));
         }
+
+
     }
 
 }
